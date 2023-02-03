a0bfc9347dbc6c2324a5b7e5d36e2dfce359c572
Olga Polishchuk <olgapolichuk@ukr.net>
2023-02-03 15:01:23 +0200

    add task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form')
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) { 
+    event.preventDefault('');
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    if(!email || !password) {alert('Всі поля повинні бути заповнені!')}
+
+    const loginFormData = {
+        email,
+        password,
+    }
+    console.log(loginFormData);
+    form.reset();
+
+}
