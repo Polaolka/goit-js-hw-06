c1be02f9fed37d06cb9cb55ac259d3ce75df2a16
Olga Polishchuk <olgapolichuk@ukr.net>
2023-02-03 15:23:33 +0200

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  bodyEl: document.querySelector('body'),
+  buttonChangeColorEl: document.querySelector('.change-color'),
+  spanColorEl: document.querySelector('.color'),
+}
+
+refs.buttonChangeColorEl.addEventListener('click', onButtonChangeColor)
+
+function onButtonChangeColor(event) {
+  refs.spanColorEl.textContent = `${getRandomHexColor()}`;
+  refs.bodyEl.style.backgroundColor = getRandomHexColor();
+}
+
